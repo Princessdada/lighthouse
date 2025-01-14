2de3545cfdfe92395adf7c4b348a9ac9f75c93ce
MAC <user@MACs-MacBook-Pro.local>
2025-01-14 08:21:45 +0100

    .

--- src/Components/AboutUs/AboutUs.tsx
+++ src/Components/AboutUs/AboutUs.tsx
@@ -1,20 +1,102 @@
 import React from "react";
-import { Flex, Box, Text, Image, Button, Grid } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
 
-import img1 from "../Components/Image/BG Image.png";
-import img2 from "../Components/Image/BG Image.png";
+import img1 from "../Image/Image (6).png";
 import Page1 from "./Page1";
 import Page2 from "./Page2";
-import Team from "./Team";
+import Team, { TeamMember } from "./Team";
 import Tab5 from "../MainTab/Tab5";
 import Footer from "../Footer/footer";
+
+const teamMembers: TeamMember[] = [
+  {
+    name: "Leonard John Davies",
+    role: "Cofounder, CEO",
+    image: img1,
+    socialLinks: {
+      facebook: "https://www.facebook.com",
+      twitter: "https://www.twitter.com",
+      linkedin: "https://www.linkedin.com",
+    },
+  },
+  {
+    name: "Francis Weber",
+    role: "Vice Chairman",
+    image: img1,
+    socialLinks: {
+      facebook: "https://www.facebook.com",
+      twitter: "https://www.twitter.com",
+      linkedin: "https://www.linkedin.com",
+    },
+  },
+  {
+    name: "Francis Weber",
+    role: "Vice Chairman",
+    image: img1,
+    socialLinks: {
+      facebook: "https://www.facebook.com",
+      twitter: "https://www.twitter.com",
+      linkedin: "https://www.linkedin.com",
+    },
+  },
+  {
+    name: "Francis Weber",
+    role: "Vice Chairman",
+    image: img1,
+    socialLinks: {
+      facebook: "https://www.facebook.com",
+      twitter: "https://www.twitter.com",
+      linkedin: "https://www.linkedin.com",
+    },
+  },
+  {
+    name: "Francis Weber",
+    role: "Vice Chairman",
+    image: img1,
+    socialLinks: {
+      facebook: "https://www.facebook.com",
+      twitter: "https://www.twitter.com",
+      linkedin: "https://www.linkedin.com",
+    },
+  },
+  {
+    name: "Francis Weber",
+    role: "Vice Chairman",
+    image: img1,
+    socialLinks: {
+      facebook: "https://www.facebook.com",
+      twitter: "https://www.twitter.com",
+      linkedin: "https://www.linkedin.com",
+    },
+  },
+  {
+    name: "Francis Weber",
+    role: "Vice Chairman",
+    image: img1,
+    socialLinks: {
+      facebook: "https://www.facebook.com",
+      twitter: "https://www.twitter.com",
+      linkedin: "https://www.linkedin.com",
+    },
+  },
+  {
+    name: "Francis Weber",
+    role: "Vice Chairman",
+    image: img1,
+    socialLinks: {
+      facebook: "https://www.facebook.com",
+      twitter: "https://www.twitter.com",
+      linkedin: "https://www.linkedin.com",
+    },
+  },
+];
 
 const AboutUs = () => {
   return (
     <Box fontFamily={"Roboto"}>
       <Page1></Page1>
       <Page2></Page2>
-      <Team></Team>
+      <Team members={teamMembers}></Team>
       <Tab5></Tab5>
       <Footer></Footer>
     </Box>
--- src/Components/AboutUs/Team.tsx
+++ src/Components/AboutUs/Team.tsx
@@ -1,7 +1,88 @@
 import React from "react";
+import {
+  Box,
+  Heading,
+  Text,
+  Grid,
+  Image,
+  Link,
+  Icon,
+  IconButton,
+} from "@chakra-ui/react";
+import { LuFacebook, LuTwitter, LuLinkedin } from "react-icons/lu";
 
-const Team = () => {
-  return <div>Team</div>;
+export type TeamMember = {
+  name: string;
+  role: string;
+  image: string;
+  socialLinks: {
+    facebook: string;
+    twitter: string;
+    linkedin: string;
+  };
+};
+
+type TeamProps = {
+  members: TeamMember[];
+};
+
+const Team: React.FC<TeamProps> = ({ members }) => {
+  return (
+    <Box p={10} textAlign="center" bg={"#fff"} color={"#1D2130"}>
+      <Heading as="h2" size="xl" mb={4}>
+        Meet our team
+      </Heading>
+      <Text fontSize="lg" mb={8}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+        varius enim in eros elementum tristique.
+      </Text>
+      <Grid templateColumns={["1fr", "1fr 1fr", "repeat(4, 1fr)"]} gap={6}>
+        {members.map((member, index) => (
+          <Box key={index} overflow="hidden" p={4} textAlign="center">
+            <Image src={member.image} alt={member.name} mx="auto" mb={4} />
+            <Heading as="h3" size="md" mb={2}>
+              {member.name}
+            </Heading>
+            <Text fontSize="sm" mb={4}>
+              {member.role}
+            </Text>
+            <Box display="flex" justifyContent="center" gap={4}>
+              {/* <IconButton
+                as="a"
+                href={member.socialLinks.facebook}
+                target="_blank"
+                rel="noopener noreferrer"
+                aria-label="Facebook"
+                icon={<LuFacebook />}
+                size="lg"
+                variant="ghost"
+              />
+              <IconButton
+                as="a"
+                href={member.socialLinks.twitter}
+                target="_blank"
+                rel="noopener noreferrer"
+                aria-label="Twitter"
+                icon={<LuTwitter />}
+                size="lg"
+                variant="ghost"
+              />
+              <IconButton
+                as="a"
+                href={member.socialLinks.linkedin}
+                target="_blank"
+                rel="noopener noreferrer"
+                aria-label="LinkedIn"
+                icon={<LuLinkedin />}
+                size="lg"
+                variant="ghost"
+              /> */}
+            </Box>
+          </Box>
+        ))}
+      </Grid>
+    </Box>
+  );
 };
 
 export default Team;
